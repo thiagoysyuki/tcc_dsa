61373c09d5cc9816ef775c43cee99921cd240156
thiagoysyuki <thiagoyukipdi@gmail.com>
2025-04-27 20:33:13 -0300

    Final Project

--- app/app.py
+++ app/app.py
@@ -1,15 +1,415 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from pypfopt.expected_returns import mean_historical_return, ema_historical_return
+import scipy.stats as stats
+from misc.otm_oportfolio import var_gaussian,cvar_historic, semideviation
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt.risk_models import sample_cov
+from pypfopt import plotting
+from plotnine import *
+from misc.otm_oportfolio import backtest_markowitz
+import misc.edhec_risk as erk
+import statsmodels.api as sm
 
-pg = st.navigation(
-    [
-        st.Page("analise_retornos.py", title="1 - Análise Histórica dos Retornos", icon=":material/analytics:"),
-        st.Page("analise_riscos.py", title="2 - Análise Histórica dos riscos", icon=":material/analytics:"),
-        st.Page("otimizacao_mv.py", title="3 - Otimização Portifólio", icon=":material/analytics:")
-    ]
-)
+url_indices ="https://drive.google.com/uc?export=download&id=14Pmao4Gp_MfjBrK7rva5OwLSBuIk-EXa"
+url_stocks = "https://drive.google.com/uc?export=download&id=1g_yX6EsOAPNBJf5NIrcMnCqQ_FXhtGdw"
 
-if "filtered_data" not in st.session_state:
-    st.session_state.filtered_data = None
+selic = pd.read_parquet(path= 'data/raw/selic/selic.parquet')
+selic.set_index('date', inplace=True)
+selic['value'] = selic['value'].astype(float)
+stocks = pd.read_parquet(path= url_stocks)
+indices = pd.read_parquet(path= url_indices)
+indices.columns = indices.columns.str.replace('^', '', regex=False)
+trading_days = stocks.index.to_series().groupby(stocks.index.year).nunique()
+trading_days = pd.DataFrame({'ano':trading_days.index,'dias_uteis':trading_days.values})
+tickers = stocks.columns
+indexes = indices.columns
 
 
-pg.run()
+with st.sidebar:
+    st.image("app/img/logo_app.png")
+    st.write("## Otimizador de Investimentos")
+    st.write("Feito por Thiago Yuki")
+    st.write("### Selecione o perído de Análise")
+    seletor_stock = st.multiselect("Ações", tickers,placeholder="Selecione as ações", default=['ITUB4', 'B3SA3', 'PETR4'])
+    seletor_index = st.multiselect("Índices", indexes,placeholder="Selecione os índices")
+    seletor_selic = st.slider("Selic", min_value=selic['value'].min(),max_value=selic['value'].max())
+    sel_data = st.date_input("Selecione o intervalo de datas", value=(stocks.index.min(), stocks.index.max()), format="DD/MM/YYYY")
+
+
+#Filtrar dados
+filtered_index_data = indices[seletor_index]
+filtered_stock_data = stocks[seletor_stock]
+filtered_stock_data = filtered_stock_data.loc[sel_data[0]:sel_data[1]]
+filtered_index_data = filtered_index_data.loc[sel_data[0]:sel_data[1]]
+filtered_data = pd.merge(filtered_stock_data, filtered_index_data, left_index=True, right_index=True, how='inner')
+
+###--------------------------------------------------------------------------------------------------------##
+
+Retornos, Riscos, Otimização = st.tabs(["Análise dos Retornos","Análise Riscos","Otimização"])
+
+with Retornos:    
+    simple_returns = filtered_data.pct_change().dropna()
+    log_returns = np.log(filtered_data / filtered_data.shift(1)).dropna()
+
+    cum_return = filtered_data.copy()
+
+    for i in range(cum_return.shape[1]):  # Itera pelas colunas usando índices
+        cum_return.iloc[:, i] = cum_return.iloc[:, i] / cum_return.iloc[0, i]
+
+    st.write("Retorno Total Acumulado" ) 
+    st.write(cum_return.tail(1))
+
+
+    stocks_graph = px.line(title='Retorno Acumulado', labels={'x': 'Índice (Datas)', 'y': 'Retorno simples'})
+    for column in cum_return.columns:
+        stocks_graph.add_scatter(x=cum_return.index, y=cum_return[column], mode='lines', name=column)
+
+    # Grafico das ações
+
+    st.plotly_chart(stocks_graph, use_container_width=True)
+
+    st.write("# Análise dos Retornos:")
+
+    seletor_geometric = st.selectbox("Selecione o método de cálculo", ["Geométrico", "Aritmético"])
+    if seletor_geometric == "Geométrico":
+        st.write("Retornos anualizados (Geométrico):")
+        mu = mean_historical_return(filtered_data, frequency=252, compounding=True).to_frame()
+    else:
+        st.write("Retornos anualizados (Aritmético):")
+        mu = mean_historical_return(filtered_data, frequency=252, compounding=False).to_frame()
+
+    mu = mu.reset_index()
+    mu.columns = ['Ticker', 'Retorno Anualizado']
+    st.dataframe(mu, use_container_width=True)
+
+
+    st.write("Retorno Anualizados (EMA):")
+    span_select = st.slider("Selecione o período de suavização (EMA)", min_value=1, max_value=365, value=30, step=1)
+    st.write(f"Período de suavização (EMA): {span_select} dias")
+    ema_return = ema_historical_return(filtered_data, span=span_select, frequency=250).to_frame()
+    ema_return = ema_return.reset_index()
+    ema_return.columns = ['Ticker', 'Retorno Anualizado']
+    st.dataframe(ema_return, use_container_width=True)
+
+    st.write("Taxa SELIC")
+
+    selic_graph = px.line(selic, x=selic.index,y=selic['value'], title='Taxa SELIC', labels={'x': 'Índice (Datas)', 'y': 'Taxa SELIC'})
+    st.plotly_chart(selic_graph, use_container_width=True)
+
+with Riscos:
+    simples, log = st.tabs(["Simples","Logarítmico"])
+
+    with simples:
+
+        st.write("# Análise de Riscos")
+
+        retorno_anualizado = pd.DataFrame(mean_historical_return(filtered_data, log_returns=False, frequency=252))
+        retorno_anualizado.reset_index(inplace=True)
+        retorno_anualizado.columns = ['Ações','Retorno Anualizado']
+        volatilidade_anualizada = pd.DataFrame(simple_returns.std() * 252 ** 0.5)
+        volatilidade_anualizada.reset_index(inplace=True)
+        volatilidade_anualizada.columns = ['Ações','Volatilidade Anualizada']
+        dados_anualizados = pd.merge(retorno_anualizado, volatilidade_anualizada, on='Ações')
+        dados_anualizados['Sharpe Ratio'] = (dados_anualizados['Retorno Anualizado'] - seletor_selic) / dados_anualizados['Volatilidade Anualizada']
+
+            # Add a regression line to the scatter plot
+        fig = px.scatter(
+            title='Retorno vs. Volatilidade',
+            data_frame=dados_anualizados,
+            x='Retorno Anualizado',
+            y='Volatilidade Anualizada',
+            labels={'x': 'Retorno Anual', 'y': 'Volatilidade Anual'},
+            trendline="ols",  # Add Ordinary Least Squares (OLS) regression line
+            trendline_color_override='red',  # Change the color of the regression line
+
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+        st.dataframe(dados_anualizados, use_container_width=True)
+
+        tabs_desity = st.tabs(list(simple_returns.columns))
+        for i, tab_name in enumerate(simple_returns.columns):
+            with tabs_desity[i]:
+                fig, ax = plt.subplots()
+                sns.kdeplot(data=simple_returns[tab_name], ax=ax, label='Kernel Density', color='blue')
+                sns.histplot(data=simple_returns[tab_name], ax=ax, bins=30, kde=True, stat='density', alpha=0.5)
+                ax.set_title(f"Distribuição de Retornos - {tab_name}")
+                ax.legend()
+                st.pyplot(fig)
+
+        st.write("## Desvios da Normalidade")
+
+        skew_table = pd.DataFrame(columns=[
+            'Ação', 'Média', 'Desvio Padrão', 'Skewness', 'Kurtosis', 
+            'Jarque-Bera', 'p-value', 'Distribuição'
+        ])
+
+        for column in simple_returns.columns:
+            row = {
+                'Ação': column,
+                'Média': simple_returns[column].mean(),
+                'Desvio Padrão': simple_returns[column].std(),
+                'Skewness': simple_returns[column].skew(),
+                'Kurtosis': simple_returns[column].kurtosis(),
+                'Jarque-Bera': stats.jarque_bera(simple_returns[column])[0],
+                'p-value': stats.jarque_bera(simple_returns[column])[1],
+                'Distribuição': 'Normal' if abs(simple_returns[column].skew()) < 0.5 and abs(simple_returns[column].kurtosis()) < 3 else 'Não Normal'
+            }
+            skew_table = pd.concat([skew_table, pd.DataFrame([row])], ignore_index=True)
+
+        # Format the 'p-value' column to scientific notation
+        skew_table['p-value'] = skew_table['p-value'].apply(lambda x: f"{x:.2e}")
+
+        st.write(skew_table)
+
+        st.write("## Drawdown Máximo")	
+
+        wealth_index =  (1 + simple_returns).cumprod()
+        previous_peaks = wealth_index.cummax()
+        drawdown = (wealth_index - previous_peaks) / previous_peaks
+
+        tabs = st.tabs(list(drawdown.columns))
+        for i, tab_name in enumerate(drawdown.columns):
+            with tabs[i]:
+                fig, ax = plt.subplots()
+                ax.plot(drawdown.index, drawdown[tab_name], label='Quedas', color='red')
+                ax.plot(previous_peaks.index, previous_peaks[tab_name], label='Picos', color='green', linestyle='--', alpha=0.5)
+                ax.plot(wealth_index.index, wealth_index[tab_name], label='Índice Saúde', color='blue')
+                ax.vlines(drawdown.index[drawdown[tab_name] == drawdown[tab_name].min()], ymin=0, ymax=drawdown[tab_name].min(), color='black', lw=1, ls='--')
+                ax.axhline(0, color='black', lw=1, ls='--')
+                ax.fill_between(drawdown.index, drawdown[tab_name], color='red', alpha=0.5)
+                ax.set_title(f"Drawdown Máximo - {tab_name}")
+                ax.legend()
+                plt.xticks(rotation=90)  # Rotate x-axis labels 90 degrees
+                st.write(f"**{tab_name}**")
+
+                st.pyplot(fig)
+
+        st.write("## Máximo Drawdown")
+        st.write("Máximo Drawdown:")
+        max_drawndow = pd.DataFrame({'Máximo Drawdown': drawdown.min(), 'Data do Máximo Drawdown': drawdown.idxmin()})
+        max_drawndow['Data do Máximo Drawdown'] = max_drawndow['Data do Máximo Drawdown'].dt.strftime('%d/%m/%Y')
+        st.write(max_drawndow)
+
+        st.write("## Value at Risk (VaR)")
+
+        historical_var = pd.DataFrame(simple_returns.quantile(0.05))
+        historical_var.reset_index(inplace=True)
+        historical_var.columns = ['Ação', 'Histórico']
+        historical_var['Histórico'] = historical_var['Histórico'] * -1
+
+        z = stats.norm.ppf(0.05)
+        VaR_parametric = pd.DataFrame(
+            (simple_returns.mean() - z * simple_returns.std())
+        )
+
+        VaR_parametric.reset_index(inplace=True)
+        VaR_parametric.columns = ['Ação', 'Gaussiano']
+
+        VaR_fs = pd.DataFrame(var_gaussian(simple_returns, level=0.05, modified=True))
+        VaR_fs.reset_index(inplace=True)
+        VaR_fs.columns = ['Ação', 'Cornish-Fisher']
+
+        beyond_var_lg =pd.DataFrame(cvar_historic(simple_returns, level=0.05))
+        beyond_var_lg.reset_index(inplace=True)
+        beyond_var_lg.columns = ['Ação', 'Beyond VaR']
+
+        VaR_simple = pd.merge(historical_var, VaR_parametric, on='Ação')
+        VaR_simple = pd.merge(VaR_simple, VaR_fs, on='Ação')
+        VaR_simple = pd.merge(VaR_simple, beyond_var_lg, on='Ação')
+
+        VaR_graph =px.bar(VaR_simple, x='Ação', y=['Histórico', 'Gaussiano', 'Cornish-Fisher'], title='Value at Risk (VaR)', labels={'x': 'Ação', 'y': 'VaR'})
+        VaR_graph.update_layout(barmode='group', xaxis_title='Ação', yaxis_title='VaR')
+
+        st.plotly_chart(VaR_graph, use_container_width=True)
+
+        st.write(VaR_simple)
+
+
+
+    with log:
+
+        st.write("# Análise de Riscos")    
+
+        retorno_anualizado_lg = pd.DataFrame(mean_historical_return(filtered_data, log_returns=True, frequency=252))
+        retorno_anualizado_lg.reset_index(inplace=True)
+        retorno_anualizado_lg.columns = ['Ações','Retorno Anualizado']
+        volatilidade_anualizada_lg = pd.DataFrame(log_returns.std() * 252 ** 0.5)
+        volatilidade_anualizada_lg.reset_index(inplace=True)
+        volatilidade_anualizada_lg.columns = ['Ações','Volatilidade Anualizada']
+        dados_anualizados_lg = pd.merge(retorno_anualizado_lg, volatilidade_anualizada_lg, on='Ações')
+        dados_anualizados_lg['Sharpe Ratio'] = (dados_anualizados_lg['Retorno Anualizado'] - seletor_selic) / dados_anualizados_lg['Volatilidade Anualizada']
+
+        # Add a regression line to the scatter plot
+        fig = px.scatter(
+            title='Retorno vs. Volatilidade',
+            data_frame=dados_anualizados_lg,
+            x='Retorno Anualizado',
+            y='Volatilidade Anualizada',
+            labels={'x': 'Retorno Anual', 'y': 'Volatilidade Anual'},
+            trendline="ols",  # Add Ordinary Least Squares (OLS) regression line
+            trendline_color_override='red',  # Change the color of the regression line
+        )
+        st.plotly_chart(fig, use_container_width=True)
+        st.write(dados_anualizados_lg, use_container_width=True)
+
+        tabs_desity_lg = st.tabs(list(log_returns.columns))
+        for i, tab_name in enumerate(log_returns.columns):
+            with tabs_desity_lg[i]:
+                fig, ax = plt.subplots()
+                sns.kdeplot(data=log_returns[tab_name], ax=ax, label='Kernel Density', color='blue')
+                sns.histplot(data=log_returns[tab_name], ax=ax, bins=30, kde=True, stat='density', alpha=0.5)
+                ax.set_title(f"Distribuição de Retornos - {tab_name}")
+                ax.legend()
+                st.pyplot(fig)
+
+        st.write("## Desvios da Normalidade")
+        skew_table_lg = pd.DataFrame(columns=[
+            'Ação', 'Média', 'Desvio Padrão', 'Skewness', 'Kurtosis', 
+            'Jarque-Bera', 'p-value', 'Distribuição'
+        ])
+
+        for column in log_returns.columns:
+            row = {
+                'Ação': column,
+                'Média': log_returns[column].mean(),
+                'Desvio Padrão': log_returns[column].std(),
+                'Skewness': log_returns[column].skew(),
+                'Kurtosis': log_returns[column].kurtosis(),
+                'Jarque-Bera': stats.jarque_bera(log_returns[column])[0],
+                'p-value': stats.jarque_bera(log_returns[column])[1],
+                'Distribuição': 'Normal' if abs(log_returns[column].skew()) < 0.5 and abs(log_returns[column].kurtosis()) < 3 else 'Não Normal',
+                'Desvio Semi-condicional': semideviation(log_returns[column])
+
+            }
+            skew_table_lg = pd.concat([skew_table_lg, pd.DataFrame([row])], ignore_index=True)
+
+        # Format the 'p-value' column to scientific notation
+        skew_table_lg['p-value'] = skew_table_lg['p-value'].apply(lambda x: f"{x:.2e}")
+
+        st.write(skew_table_lg)
+
+        st.write("## Drawdown Máximo")	
+
+        wealth_index_lg =  (1 + log_returns).cumprod()
+        previous_peaks_lg = wealth_index.cummax()
+        drawdown_lg = (wealth_index - previous_peaks) / previous_peaks
+
+        tabs_lg = st.tabs(list(drawdown_lg.columns))
+        for i, tab_name in enumerate(drawdown_lg.columns):
+            with tabs_lg[i]:
+                fig, ax = plt.subplots()
+                ax.plot(drawdown_lg.index, drawdown_lg[tab_name], label='Quedas', color='red')
+                ax.plot(previous_peaks.index, previous_peaks[tab_name], label='Picos', color='green', linestyle='--', alpha=0.5)
+                ax.plot(wealth_index.index, wealth_index[tab_name], label='Índice Saúde', color='blue')
+                ax.vlines(drawdown_lg.index[drawdown_lg[tab_name] == drawdown_lg[tab_name].min()], ymin=0, ymax=drawdown_lg[tab_name].min(), color='black', lw=1, ls='--')
+                ax.axhline(0, color='black', lw=1, ls='--')
+                ax.fill_between(drawdown_lg.index, drawdown_lg[tab_name], color='red', alpha=0.5)
+                ax.set_title(f"Drawdown Máximo - {tab_name}")
+                ax.legend()
+                plt.xticks(rotation=90)  # Rotate x-axis labels 90 degrees
+                st.write(f"**{tab_name}**")
+
+                st.pyplot(fig)
+
+        st.write("## Máximo Drawdown")
+        st.write("Máximo Drawdown:")
+        max_drawndow_lg = pd.DataFrame({'Máximo Drawdown': drawdown_lg.min(), 'Data do Máximo Drawdown': drawdown_lg.idxmin()})
+        max_drawndow_lg['Data do Máximo Drawdown'] = max_drawndow_lg['Data do Máximo Drawdown'].dt.strftime('%d/%m/%Y')
+
+        st.write(max_drawndow_lg)
+
+        st.write("## Value at Risk (VaR)")
+
+        historical_var_lg = pd.DataFrame(log_returns.quantile(0.05))
+        historical_var_lg.reset_index(inplace=True)
+        historical_var_lg.columns = ['Ação', 'Histórico']
+        historical_var_lg['Histórico'] = historical_var['Histórico'] * -1
+
+        z = stats.norm.ppf(0.05)
+        VaR_parametric_lg = pd.DataFrame(
+            (log_returns.mean() - z * log_returns.std())
+        )
+
+        VaR_parametric_lg.reset_index(inplace=True)
+        VaR_parametric_lg.columns = ['Ação', 'Gaussiano']
+
+        VaR_fs_lg = pd.DataFrame(var_gaussian(log_returns, level=0.05, modified=True))
+        VaR_fs_lg.reset_index(inplace=True)
+        VaR_fs_lg.columns = ['Ação', 'Cornish-Fisher']
+
+        beyond_var_lg =pd.DataFrame(cvar_historic(log_returns, level=0.05))
+        beyond_var_lg.reset_index(inplace=True)
+        beyond_var_lg.columns = ['Ação', 'Beyond VaR']
+
+        VaR_log = pd.merge(historical_var_lg, VaR_parametric_lg, on='Ação')
+        VaR_log = pd.merge(VaR_log, VaR_fs_lg, on='Ação')
+        VaR_log = pd.merge(VaR_log, beyond_var_lg, on='Ação')
+
+        VaR_graph =px.bar(VaR_log, x='Ação', y=['Histórico', 'Gaussiano', 'Cornish-Fisher'], title='Value at Risk (VaR)', labels={'x': 'Ação', 'y': 'VaR'})
+        VaR_graph.update_layout(barmode='group', xaxis_title='Ação', yaxis_title='VaR')
+
+        st.plotly_chart(VaR_graph, use_container_width=True)
+
+        st.write(VaR_log)
+
+        semideviation(log_returns)
+
+
+with Otimização:
+    st.write("## Fronteira Eficiênte")
+
+    data_historico = st.date_input(label="Período Historico", value=(filtered_data.index.min(), filtered_data.index.max()), format="DD/MM/YYYY")
+    historico = filtered_data[data_historico[0]:data_historico[1]]
+
+    data_backtest = st.date_input(label="Período de Backtest", value=(filtered_data.index.min(), filtered_data.index.max()), format="DD/MM/YYYY")
+    backtest = filtered_data[data_backtest[0]:data_backtest[1]]
+
+    investimento_input = st.number_input(label="Investimento R$", value=1000)
+
+    experimento = backtest_markowitz(prices=historico, backtest=backtest,investimento=investimento_input, risk_free=seletor_selic)
+    experimento.optimization_mv()
+    experimento.backtest_performance()
+
+    weights = experimento.weights
+    performance = experimento.performance
+    retornos_cumulativos = experimento.cum_return
+
+    fig,ax = experimento.fronteira_eficiente_plot()
+    st.pyplot(fig)
+
+    weights_long = weights.reset_index().melt(id_vars= "Ticker",var_name='Carteira',value_name='Fração Percentual') 
+    weights_long['Fração Percentual'] = round(weights_long['Fração Percentual'] * 100,2)
+
+    st.write("## Composição das Carteiras")
+
+    bart_carteira = ggplot(weights_long, aes(x="Carteira", y="Fração Percentual", fill="Ticker"))+ geom_col()
+
+    st.pyplot(ggplot.draw(bart_carteira))
+
+    st.write("## BackTest da Carteira")
+
+    st.write(performance)    
+
+    evol_retornos = px.line(title="Retornos Cumulativos")
+    for col in retornos_cumulativos:
+        evol_retornos.add_scatter(x=retornos_cumulativos.index, y=retornos_cumulativos[col], mode="lines", name=col)
+
+    st.plotly_chart(evol_retornos)
+
+   
+
+
+
+
+
+
+
+
+
+
--- app/misc/otm_oportfolio.py
+++ app/misc/otm_oportfolio.py
@@ -2,6 +2,131 @@
 from scipy.stats import norm, skew, kurtosis
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pypfopt.expected_returns import mean_historical_return
+from pypfopt.risk_models import sample_cov
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import plotting
+
+
+class backtest_markowitz:
+
+    def __init__(self, prices, backtest, investimento, mu = None, S=None, weights=None, cum_return=None, init_portfolio=None, performance=None, risk_free=None):
+        self.prices = prices
+        self.backtest = backtest
+        self.investimento = investimento
+        self.weights = weights
+        self.cum_return = cum_return
+        self.init_portfolio= init_portfolio
+        self.mu = mean_historical_return(self.prices, frequency=252, compounding=True)
+        self.S = sample_cov(self.prices, frequency=252)
+        self.performance = performance
+        self.risk_free = risk_free
+        
+        
+    def optimization_mv(self):
+        portfolios= {"gmv":"Global Minimum Variance",
+                    "msr": "Maximum Sharpe Ratio"}            
+        gmv = []
+        msr = []
+
+        for ef_port in portfolios.keys():
+            if ef_port == "gmv":
+                ef = EfficientFrontier(self.mu, self.S, weight_bounds=(0, 1))
+                portfolio = ef.min_volatility()
+                gmv.append(portfolio)
+            else:
+                ef = EfficientFrontier(self.mu, self.S, weight_bounds=(0, 1))
+                portfolio = ef.max_sharpe(risk_free_rate=self.risk_free/100)
+                msr.append(portfolio)
+
+        mgv = pd.DataFrame(list(gmv[0].items()), columns=['Ticker', 'MGV'])
+        msr = pd.DataFrame(list(msr[0].items()), columns=['Ticker', 'MSR'])
+
+        # Merge the two DataFrames on the 'Ticker' column
+        merged_df = pd.merge(mgv, msr, on='Ticker')
+        merged_df["EW"] = 1/ merged_df.shape[0]
+        merged_df.set_index('Ticker', inplace=True, drop=True)         
+           
+        self.weights = merged_df
+    
+    def backtest_performance(self):
+        backtest = self.backtest
+        for i in range(backtest.shape[1]):  # Itera pelas colunas usando índices
+            backtest.iloc[:, i] = backtest.iloc[:, i] / backtest.iloc[0, i]
+        
+        start_investment_msr = []
+        start_investment_mv = []
+        start_investment_ew = []
+        
+
+        for portfolio in self.weights.columns:
+            if portfolio == 'MGV':                
+                posicao_inicial =  self.weights[portfolio] * self.investimento
+                start_investment_mv.append(posicao_inicial)   
+            elif portfolio == 'MSR':
+                posicao_inicial =  self.weights[portfolio] * self.investimento
+                start_investment_msr.append(posicao_inicial)
+            else:
+                posicao_inicial =  self.weights[portfolio] * self.investimento
+                start_investment_msr.append(posicao_inicial) 
+        
+        start_investment_mv = pd.DataFrame(start_investment_mv)
+        start_investment_msr = pd.DataFrame(start_investment_msr)
+        start_investment_ew = pd.DataFrame(start_investment_ew)
+
+        initial_position = pd.concat([start_investment_mv,start_investment_msr, start_investment_ew])
+        
+        evol_mgv = initial_position.loc["MGV",:] * backtest
+        evol_msr = initial_position.loc["MSR",:] * backtest
+        evol_ew = initial_position.loc["EW",:] * backtest
+
+        evol_mgv['Total carteira'] = evol_mgv.sum(axis=1)
+        evol_msr['Total carteira'] = evol_msr.sum(axis=1)
+        evol_ew['Total carteira'] = evol_ew.sum(axis=1)
+
+        self.cum_return = pd.DataFrame({"Carteira MSR": evol_msr['Total carteira'],"Carteira MGV": evol_mgv['Total carteira'],"Carteira EW": evol_ew['Total carteira']})
+
+        self.performance = pd.DataFrame({"Retorno percentual (%)":round((self.cum_return.iloc[-1] - self.cum_return.iloc[0]) /  self.cum_return.iloc[-1],2) * 100,
+                                         "Retorno Real R$": round(self.cum_return.iloc[-1] - self.cum_return.iloc[0],2)})
+        
+    
+    def fronteira_eficiente_plot(self):
+        ef = EfficientFrontier(self.mu, self.S, weight_bounds=(0, 1))
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ef_max_sharpe = ef.deepcopy()
+        plotting.plot_efficient_frontier(ef, ax=ax, show_assets=True, show_tickers=True, )
+
+        # Find the tangency portfolio
+        ef_max_sharpe.max_sharpe(risk_free_rate= self.risk_free/100)
+        ret_tangent, std_tangent, _ = ef_max_sharpe.portfolio_performance()
+        ax.scatter(std_tangent, ret_tangent, marker="*", s=100, c="r", label="Max Sharpe")
+
+        # Generate random portfolios
+        n_samples = 30000
+        w = np.random.dirichlet(np.ones(ef.n_assets), n_samples)
+        rets = w.dot(ef.expected_returns)
+        stds = np.sqrt(np.diag(w @ ef.cov_matrix @ w.T))
+        sharpes = rets / stds
+        ax.scatter(stds, rets, marker=".", c=sharpes, cmap="viridis_r")
+
+        # Output
+        ax.set_title(f"Fronteira Eficiênte com {n_samples} carteiras aleatórias")
+        ax.set_xlabel("Volatilidade")
+        ax.set_ylabel("Retornos")
+        ax.legend()
+        plt.tight_layout()
+        return fig, ax
+      
+
+    
+    def start_opt(self):
+        self.optimization_mv(self)
+        self.backtest_performance(self)
+        self.fronteira_eficiente_plot(self)
+
+
 
 def semideviation(r):
     if isinstance(r, pd.DataFrame):
